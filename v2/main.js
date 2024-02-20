86918d8b128fef46df7b5f8a7093a348baca6c22
강현우(Kane Kang) <kanghw@LGDISPLAY.GLOBAL>
2024-02-19 22:56:15 -0800

    021924_v4

--- v2/main.js
+++ v2/main.js
@@ -1,7 +1,7 @@
 let news = [];
 
 const getLatestNews = async () => {
-  const url = new URL(`https://flourishing-moxie-2c1179.netlify.app`);
+  const url = new URL(`https://gleeful-centaur-a41938.netlify.app`);
   const response = await fetch(url);
   const data = await response.json();
   news = data.articles;
